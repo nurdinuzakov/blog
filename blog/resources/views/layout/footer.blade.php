cc91c749d8379c2633a065a733f1a6d85e86d2cc
Nurdin <nurdinuzakov@gmail.com>
2021-04-19 18:19:57 +0600

    created posts list

---
+++ blog/resources/views/layout/footer.blade.php
@@ -0,0 +1,78 @@
+<div class="wrapper row4">
+    <footer id="footer" class="hoc clear">
+        <div class="center btmspace-50">
+            <!-- ################################################################################################ -->
+            <h6 class="heading">Shiphile</h6>
+            <ul class="faico clear">
+                <li><a class="faicon-dribble" href="#"><i class="fab fa-dribbble"></i></a></li>
+                <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a class="faicon-google-plus" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>
+                <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a class="faicon-vk" href="#"><i class="fab fa-vk"></i></a></li>
+            </ul>
+            <p class="nospace">Nisl quis leo scelerisque dapibus sed a arcu quisque et neque id mauris</p>
+            <!-- ################################################################################################ -->
+        </div>
+        <hr class="btmspace-50">
+        <div class="group">
+            <!-- ################################################################################################ -->
+            <div class="one_quarter first">
+                <h6 class="heading">Tristique tincidunt</h6>
+                <ul class="nospace btmspace-30 linklist contact">
+                    <li><i class="fas fa-map-marker-alt"></i>
+                        <address>
+                            Street Name &amp; Number, Town, Postcode/Zip
+                        </address>
+                    </li>
+                    <li><i class="fas fa-phone"></i> +00 (123) 456 7890</li>
+                    <li><i class="far fa-envelope"></i> info@domain.com</li>
+                    <li><i class="fas fa-clock"></i> Mon. - Sat.:<br>
+                        08.00am - 18.00pm</li>
+                </ul>
+            </div>
+            <div class="one_quarter">
+                <h6 class="heading">Praesent lorem nulla</h6>
+                <ul class="nospace linklist">
+                    <li><a href="#">Ligula risus porttitor</a></li>
+                    <li><a href="#">Ac rutrum id euismod in diam</a></li>
+                    <li><a href="#">Nunc ultricies faucibus</a></li>
+                    <li><a href="#">Nullam risus lectus</a></li>
+                    <li><a href="#">Venenatis eu vulputate</a></li>
+                </ul>
+            </div>
+            <div class="one_quarter">
+                <h6 class="heading">Feugiat porta sed ipsum</h6>
+                <ul class="nospace linklist">
+                    <li>
+                        <article>
+                            <p class="nospace btmspace-10"><a href="#">Proin rutrum turpis non accumsan aliquet odio magna luctus neque in adipiscing.</a></p>
+                            <time class="block font-xs" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
+                        </article>
+                    </li>
+                    <li>
+                        <article>
+                            <p class="nospace btmspace-10"><a href="#">Mi odio ac felis nunc vitae lacus sit amet sem consequat ullamcorper quisque.</a></p>
+                            <time class="block font-xs" datetime="2045-04-05">Thursday, 5<sup>th</sup> April 2045</time>
+                        </article>
+                    </li>
+                </ul>
+            </div>
+            <div class="one_quarter">
+                <h6 class="heading">Posuere suscipit massa</h6>
+                <ul class="nospace clear latestimg">
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                    <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                </ul>
+            </div>
+            <!-- ################################################################################################ -->
+        </div>
+    </footer>
+</div>
